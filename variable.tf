4bdfffa2580199b624587de2b50537a133f67f83
Anjina Reddy <anjinadev24@gmail.com>
2025-05-30 12:32:53 +0530

    adding aws vpc terraform code

--- variable.tf
+++ variable.tf
@@ -1,8 +1,55 @@
-variable "aws_region" {
-    description = "The AWS region to deploy resources in"
-    type        = string
-    default     = "us-east-1"
+variable "cidr_block" {
+  #default = "10.0.0.0/0"
+}
 
+variable "enable_dns_hostnames" {
+  default = true
+}
 
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+  default = {} # this means tags is optional
+  type = map
+}
+
+variable "vpc_tags" {
+  default = {} # this means tags is optional
+  type = map
+}
+
+variable "igw_tags" {
+  default = {}
+}
+
+variable "public_subnet_cidr" {
   
 }
+
+variable "public_subnet_names" {
+  
+}
+
+variable "azs" {
+  
+}
+
+variable "private_subnet_cidr" {
+  
+}
+
+variable "private_subnet_names" {
+  
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+variable "private_route_table_tags" {
+  
+}
+
+
